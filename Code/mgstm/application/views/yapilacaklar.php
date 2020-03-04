ae75638c6eb72086ae160f70ee1521f9e45c62ad
MertCetinkya <43878831+MertCetinkya@users.noreply.github.com>
2020-03-04 11:59:06 +0300

    mert commit
    boş array yapısı düzeltildi

--- Code/mgstm/application/views/yapilacaklar.php
+++ Code/mgstm/application/views/yapilacaklar.php
@@ -6,42 +6,53 @@ print_r($query);
 echo "<br>";
 echo "<br>";
 
-foreach($query as $meyve) { 
-	print_r ($meyve);
-	$sıra_no=$meyve->Id;
-	echo "<br>";
-	$tanim=$meyve->Tanim;
-	echo $tanim;
-	echo "<br>";
-	$aciklama=$meyve->Aciklama;
-	echo $aciklama;
-	echo "<br>";
-	$state=$meyve->State;
-	echo $meyve->State;
-	echo "<br>";
-	echo "<br>";
+
+if ($query!=null){
+
+	foreach($query as $meyve) { 
+		print_r ($meyve);
+		$sıra_no=$meyve->Id;
+		echo "<br>";
+		$tanim=$meyve->Tanim;
+		echo $tanim;
+		echo "<br>";
+		$aciklama=$meyve->Aciklama;
+		echo $aciklama;
+		echo "<br>";
+		$state=$meyve->State;
+		echo $meyve->State;
+		echo "<br>";
+		echo "<br>";
+		?>
+		<form action="#" method="post" style="border-color: red; border:dotted; padding: 10px;">
+			<input style="visibility: hidden;" type="text" name="sirano" value="<?php echo $sıra_no ?>" >
+			<br/>
+			<br/>
+			<input type="text" name="Tanim" value="<?php echo $tanim ?>">
+			<br/>
+			<br/>
+			<input type="text" name="Aciklama" value="<?php  echo $aciklama ?>">
+			<br/>
+			<br/>
+			<input type="radio" name="State" value="Yapıldı"> Yapıldı!<br>
+			<input type="radio" name="State" value="Yapılmadı"> sYapılmadı<br>
+			<br/>
+			<br/>
+			<button type="submit" formaction="UpdatePage"> Düzele</button>
+			<button type="submit" formaction="DeletePage" style="padding-left:10px; "> Sil</button>
+			<button type="submit" formaction="Yapilacaklarekle">Yeni Yapılacak İş Ekle</button>
+		</form>
+		<?php 
+	} 
 	?>
-	<form action="#" method="post" style="border-color: red; border:dotted; padding: 10px;">
-		<input style="visibility: hidden;" type="text" name="sirano" value="<?php echo $sıra_no ?>" >
-		<br/>
-		<br/>
-		<input type="text" name="Tanim" value="<?php echo $tanim ?>">
-		<br/>
-		<br/>
-		<input type="text" name="Aciklama" value="<?php  echo $aciklama ?>">
-		<br/>
-		<br/>
-		<input type="radio" name="State" value="Yapıldı"> Yapıldı!<br>
-		<input type="radio" name="State" value="Yapılmadı"> sYapılmadı<br>
-		<br/>
-		<br/>
-		<button type="submit" formaction="UpdatePage"> Düzele</button>
-		<button type="submit" formaction="DeletePage" style="padding-left:10px; "> Sil</button>
-		<button type="submit" formaction="Yapilacaklarekle">Yeni Yapılacak İş Ekle</button>
-	</form>
+	<?php
+}
+
+else{
+	?>
+	<form action="#" method="post">
+			<button type="submit" formaction="Yapilacaklarekle">Yeni Yapılacak İş Ekle</button>
 	<?php 
-} 
+}
 ?>
-
-
 </html>
